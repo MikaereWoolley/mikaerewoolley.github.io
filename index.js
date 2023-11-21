25fe3514d796d824cf4266a56f09d6c043f6df63
Mikaere Woolley <mikaerewoolley@gmail.com>
2023-11-21 19:23:11 +1300

    A new start

--- index.js
+++
@@ -1,5 +0,0 @@
-var descript = document.getElementsByClassName('descript');
-var proj = document.getElementsByClassName('project');
-var att = document.createAttribute('max-width');
-att.value = '100%'
-
